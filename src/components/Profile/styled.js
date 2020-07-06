000a6fc081677b59fc0968ae51b2adcf4518a60e
Monique Azevedo da Silva <monique.azevedo@BRRJOBITAIM2724.local>
2020-07-05 21:46:40 -0300

    Projeto curso gatsby

---
+++ src/components/Profile/styled.js
@@ -0,0 +1,34 @@
+import styled from "styled-components"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
+
+export const ProfileWrapper = styled.section`
+  color: var(--texts);
+  display: flex;
+  flex-direction: column;
+`
+
+export const ProfileLink = styled(AniLink)`
+  color: var(--texts);
+  text-decoration: none;
+  transition: color 0.5s;
+  &:hover {
+    color: var(--highlight);
+  }
+`
+
+export const ProfileAuthor = styled.h1`
+  font-size: 1.6rem;
+  margin: 0.5rem auto 1.5rem;
+`
+
+export const ProfilePosition = styled.small`
+  display: block;
+  font-size: 1.2rem;
+  font-weight: 300;
+`
+
+export const ProfileDescription = styled.p`
+  font-size: 1rem;
+  font-weight: 300;
+  line-height: 1.4;
+`
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/Seo"
+
+const AboutPage = () => (
+  <Layout>
+    <SEO title="About" />
+    <h1>Abount Page</h1>
+  </Layout>
+)
+
+export default AboutPage
